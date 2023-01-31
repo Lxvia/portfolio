999cd6d77365fe77ef8ffd37affa621a93c402ca
Olivia <oliviarudolf67@gmail.com>
2023-01-31 19:19:46 +0100

    add home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import olivia from "../assets/images/oliviaportfolio.png";
 
 const Home = () => {
-    return (
+  return (
+    <div className="home">
+      <div className="home-left">
+        <img src={olivia} alt="photo de olivia" />
+      </div>
+      <div className="home-right">
+        <h1>
+          <span>Olivia,</span>
+          <br />
+          Developpeuse<br/> Full Stack &<br /> Web designer
+        </h1>
         <div>
-            <h1>Mon portfolio</h1>
+          <button>Contact</button>
+          <a href="/">Télécharger mon cv</a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
