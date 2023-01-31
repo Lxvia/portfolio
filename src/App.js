8d3b6fb79488b84d86fbebe318fc3792f186f48d
Olivia <oliviarudolf67@gmail.com>
2023-01-31 17:44:42 +0100

    add all pages routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './pages/About';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 const App = () => {
   return (
     <BrowserRouter>
+     <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
