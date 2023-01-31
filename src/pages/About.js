7f3073b2bad77b78b767e7479991d4f2e8a807e5
Olivia <oliviarudolf67@gmail.com>
2023-01-31 17:45:25 +0100

    create pages

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Du design au code pour vous accompagner dans la création de votre site web.</h1>
+        </div>
+    );
+};
+
+export default About;
