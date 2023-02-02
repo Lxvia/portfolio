8131418c105c788fd2cc735d8f892e46d2719e52
Olivia <oliviarudolf67@gmail.com>
2023-02-02 18:36:48 +0100

    about ok

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import Timeline from '../components/Timeline';
+import React from "react";
+import Timeline from "../components/Timeline";
 
 const About = () => {
-    return (
-        <div>
-            <h1>Du design au code pour vous accompagner dans la création de votre site web.</h1>
-            <Timeline />
-        </div>
-    );
+  return (
+    <div>
+      <div className="about">
+        <h1>
+          Du design au code pour vous accompagner dans la
+          <br /> création de votre site web.
+        </h1>
+        <p>
+          Suite à l’obtention de mon diplôme en Webdesign, j’ai souhaité élargir
+          mes
+          <br /> compétences en apprenant à coder. Je me rend vite compte que
+          j’ai trouvé ma voie et
+          <br /> décroche mon deuxième diplôme, cette fois-ci en Développement
+          web.
+          <br />
+          <br /> C’est grâce à ma polyvalence que je peux aujourd'hui vous
+          proposer un accompagnement
+          <br /> complet, allant du design au développement lors de la création
+          de votre projet.
+        </p>
+      </div>
+
+      <Timeline />
+    </div>
+  );
 };
 
 export default About;
