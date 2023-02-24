91dbee7e82a88f05e9b0f8cf403f137c345c2c1a
Olivia <oliviarudolf67@gmail.com>
2023-02-24 21:28:44 +0100

    front project page potable but not ok

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,9 +3,20 @@ import zorya from "../assets/images/mockups/mockup_zorya.png";
 import strasdev from "../assets/images/mockups/mockup_strasdev.png";
 
 const projects = [
-  { title: "Epidaure", photo: epidaure, slug:"epidaure" },
-  { title: "Zorya", photo: zorya, slug: "zorya" },
-  { title: "Stras.Dev", photo: strasdev, slug: "strasdev" },
+  {
+    title: "Epidaure",
+    photo: epidaure,
+    slug: "epidaure",
+    description:
+      "Epidaure est une application d'organisation déstinée aux professionnels de santé. Nous l'avons réalisée pour une osthéopathe rencontrant des difficultés à planifier les consultations au sein de son cabinet médical. Grâce à cette app, chaque praticien peut consulter, modifier son planning ou toutes autres informations nécessaires en temps réel. Réalisée dans le cadre de mon Bac+2 en Développement Web, ce site internet fonctionne avec une API et un Back-End en Ruby-on-Rails. Pour le Front-End, React.js et Tailwind ont été utilisés",
+  },
+  {
+    title: "Zorya",
+    photo: zorya,
+    slug: "zorya",
+    description: "Zorya est une plateforme de E-Learning déstinée aux professionnels souhaitant former leurs employés sur divers sujets en lien avec la cybersécurité. Réalisé dans le cadre de mon Bac+2 en Développement Web, Ruby-on-Rails et Tailwind css ont été utilisés pour le Back-end ainsi que le Front-End.",
+  },
+  { title: "Stras.Dev", photo: strasdev, slug: "strasdev", description: "Stras Dev est un site qui recense tous les évenenements du Grand-Est en lien avec le monde de l'informatique, qui jusqu'a présent étaient dispatchés sur plusieurs autres plateformes. Je l'ai réalisé pour Capitale Dev (Anciennement Google Developers Group Strasbourg), association dans laquelle j'occupe le poste de Développeuse Full-Stack. J'ai utilisé Firebase pour le Back-End, et React.js pour le Front-End." },
 ];
 
 export default projects;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import projects from '../../src/data/projects'
+import React from "react";
+import { useParams } from "react-router-dom";
+import projects from "../../src/data/projects";
 
 const Project = () => {
-    const { projectSlug } = useParams();
+  const { projectSlug } = useParams();
 
-    const findProject = projects.find((project) => project.slug === projectSlug)
-    
-    return (
-        <div className='project'>
-            <h1>Mon projet est {findProject.title}</h1>
-        </div>
-    );
+  const findProject = projects.find((project) => project.slug === projectSlug);
+
+  return (
+    <div className="project">
+      <div className="container">
+        <h1>{findProject.title}</h1>
+        <p>{findProject.description}</p>
+      </div>
+      <img src={findProject.photo} alt="" />
+    </div>
+  );
 };
 
 export default Project;
