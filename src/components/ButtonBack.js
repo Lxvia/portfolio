af90d7f97e3e1fc7390b5088ffe804af612b15a5
Olivia <oliviarudolf67@gmail.com>
2023-02-28 13:46:10 +0100

    add icon btn

--- src/components/ButtonBack.js
+++ src/components/ButtonBack.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ButtonBack = () => {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
-    const goBack = () => {
-        navigate(-1);
-    }
+  const goBack = () => {
+    navigate(-1);
+  };
 
-    return (
-        <div className='btn-back'>
-            <button onClick={goBack}>Retour</button>
-        </div>
-    );
+  return (
+    <div className="btn-back">
+      <button onClick={goBack}>
+        Retour
+        <span className="btn-back-icon"></span>
+        <FontAwesomeIcon icon="fa-solid fa-caret-left" />
+      </button>
+    </div>
+  );
 };
 
 export default ButtonBack;
